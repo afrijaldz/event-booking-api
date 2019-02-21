45a5372be9f8d574292dfbcb550075d7b406e314
Afrijal Dzuhri <ijuldz@gmail.com>
2019-02-21 08:06:22 +0700

    add mongodb to store data and get data

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
+const mongoose = require('mongoose')
+const Event = require('./models/event')
 
 const app = express()
-
-const events = []
 
 app.use(bodyParser.json())
 
@@ -41,22 +41,38 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return events
+      return Event.find()
+      .then(events => events.map(event => ({ ...event._doc })))
+      .catch(err => {
+        console.log(err)
+        throw err
+      })
     },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: args.eventInput.date,
-      }
-
-      events.push(event)
-      return event
+      const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+      })
+      return event.save().then(result => ({ ...result._doc }))
+      .catch(err => {
+        console.log(err)
+        throw err
+      })
     },
   },
   graphiql: true,
 }))
 
-app.listen(3000)
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-spppf.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`, {
+  useNewUrlParser: true
+})
+  .then(() => {
+    app.listen(3000, () => {
+      console.log(`server running on port 3000`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+  })
