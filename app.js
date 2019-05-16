728248f178935df1c76b52d3ba9b3afa8fec8f6e
Afrijal Dzuhri <ijuldz@gmail.com>
2019-05-16 13:42:56 +0700

    add bcrypt to hash user password

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
 const Event = require('./models/event')
+const User = require('./models/user')
 
 const app = express()
 
@@ -19,11 +22,22 @@ app.use('/graphql', graphqlHttp({
       date: String!
     }
 
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    input UserInput {
+      email: String!
+      password: String!
     }
 
     type RootQuery {
@@ -32,6 +46,7 @@ app.use('/graphql', graphqlHttp({
 
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -50,10 +65,11 @@ app.use('/graphql', graphqlHttp({
     },
     createEvent: (args) => {
       const event = new Event({
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: new Date(args.eventInput.date),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date),
+        creator: '5c6e97053cbc312e952b5457',
       })
       return event.save().then(result => ({ ...result._doc }))
       .catch(err => {
@@ -61,6 +77,31 @@ app.use('/graphql', graphqlHttp({
         throw err
       })
     },
+    createUser: (args) => {
+      return User.findOne({
+        email: args.userInput.email
+      }).then(user => {
+        if (user) {
+          throw new Error('User exists already.')
+        }
+
+        return bcrypt.hash(args.userInput.password, 12)
+      })
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword,
+          })
+
+          return user.save()
+        })
+        .then(result => {
+          return { ...result._doc, password: null }
+        })
+        .catch(err => {
+          throw err
+        })
+    }
   },
   graphiql: true,
 }))
--- models/event.js
+++ models/event.js
@@ -19,6 +19,10 @@ const eventSchema = new Schema({
     type: Date,
     required: true,
   },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  }
 })
 
 module.exports = mongoose.model('Event', eventSchema)
